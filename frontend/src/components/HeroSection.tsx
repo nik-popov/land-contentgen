d00586217d999500baaf392f0cfd9828a3c7a881
nikwifhat <apis.popov@gmail.com>
2025-04-11 13:57:35 +0000

    cta

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -35,7 +35,7 @@ function HeroSection({ title, subtitle, ctas = [], bgImage = '' }) {
       >
         <VStack
           align="flex-start"
-          spacing={6}
+          spacing={{ base: 4, md: 6 }} // Reduced spacing on mobile
           w={{ base: "100%", md: "50%" }}
           px={{ base: 0, md: 8 }}
         >
@@ -53,9 +53,36 @@ function HeroSection({ title, subtitle, ctas = [], bgImage = '' }) {
             color="white"
             fontWeight="normal"
             maxW="600px"
+            mb={6}
           >
             {subtitle}
           </Text>
+          {/* CTA buttons on mobile */}
+          <Flex
+            display={{ base: "flex", md: "none" }} // Show only on mobile
+            justify="flex-start"
+            gap={4}
+            flexWrap="wrap"
+            w="100%"
+          >
+            {ctas.map((cta, index) => (
+              <Button
+                key={cta.link}
+                as="a"
+                href={cta.link}
+                color={index === 0 ? "white" : "orange.400"}
+                bg={index === 0 ? "orange.400" : "white"}
+                size="lg"
+                _hover={{
+                  bg: index === 0 ? "white" : "orange.400",
+                  color: index === 0 ? "orange.400" : "white",
+                }}
+                px={6}
+              >
+                {cta.text}
+              </Button>
+            ))}
+          </Flex>
         </VStack>
 
         <Box
@@ -63,13 +90,15 @@ function HeroSection({ title, subtitle, ctas = [], bgImage = '' }) {
           display="flex"
           justifyContent={{ base: "center", md: "flex-end" }}
           alignItems="center"
-          minH={{ base: "300px", md: "400px" }}
+          minH={{ base: "100px", md: "400px" }} // Reduced height on mobile
         >
           {/* Image removed, but space preserved */}
         </Box>
       </Flex>
 
+      {/* CTA buttons on desktop */}
       <Flex
+        display={{ base: "none", md: "flex" }} // Show only on desktop
         justify="center"
         gap={4}
         pt={{ base: 6, md: 8 }}
@@ -88,7 +117,7 @@ function HeroSection({ title, subtitle, ctas = [], bgImage = '' }) {
             size="lg"
             _hover={{
               bg: index === 0 ? "white" : "orange.400",
-              color: index === 0 ? "orange.400" : "white"
+              color: index === 0 ? "orange.400" : "white",
             }}
             px={6}
           >
